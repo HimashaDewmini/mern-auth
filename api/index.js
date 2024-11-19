dc525cf9b6af4d6d476de09441ad5abf403237eb
HimashaDewmini <himashadewmini12345@gmail.com>
2024-11-19 15:02:55 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import  dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB');
+
+}).catch((err)=>{
+    console.log(err);
+});
+
+
 const app = express();
+
+
+
 app.listen(3000, () => {
     console.log ("server listning on port 3000!")
 });
